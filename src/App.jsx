e947463878aea033d9806da905efa21ff213bc26
sherii190 <phonetech195@gmail.com>
2023-09-29 22:24:21 +0200

    project done

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
-import { createContext, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './components/Header'
-import Home from './pages/Home'
-import { ThemeContext } from './context/ThemeContext'
-// import ThemeContext from './Context/ThemeContext'
-//export const ThemeContext = createContext('dark');
+import Home from './Pages/Home'
+import Header from './Componenets/Header'
+import { ThemeContext } from './Context/ThemeContext'
 
 function App() {
   const [count, setCount] = useState(0)
-  const [theme, setTheme] = useState('light')
-useEffect(()=>{
-   
-  setTheme(localStorage.getItem('theme')?localStorage.getItem('theme'):'dark')
-
-},[])
+  const [theme,setTheme]=useState('dark')
+  useEffect(()=>{
+   setTheme(localStorage.getItem('theme')?localStorage.getItem('theme'):'dark')
+  },[])
   return (
     <ThemeContext.Provider value={{theme,setTheme}}>
-    <div className={`${theme} ${theme=='dark'?'bg-[#121212]':null}`}>
-        <Header/>
-        <Home/>
+    <div className={`${theme} 
+    ${theme=='dark'?'bg-[#121212]':null} min-h-[100vh]`}>
+      <Header/>
+      <Home/>
     </div>
-     </ThemeContext.Provider>
+    </ThemeContext.Provider>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,50 +1,53 @@
 import React, { useEffect, useState } from 'react'
-import SideNavGenreList from '../Componets/SideNavGenreList'
-import GlobalApi from '../Servies.js/GlobalApi'
-import Slider from '../Componets/Slider'
-import PopularGameList from '../Componets/PopularGameList'
-import TrendingGameList from '../Componets/TrendingGameList'
+import GenreList from '../Componenets/GenreList'
+import GlobalApi from '../Services/GlobalApi'
+import Banner from '../Componenets/Banner';
+import TrendingGames from '../Componenets/TrendingGames';
+import GamesByGenresId from '../Componenets/GamesByGenresId';
 
 function Home() {
-    const [gameList,setGameList]=useState([])
-    const [gameListByGenre,setGameListByGenre]=useState([])
+  const [allGameList,setAllGameList]=useState();
+  const [gameListByGenres,setGameListByGenres]=useState([]);
+  const [selctedGenresName,setSelctedGenresName]=useState('Action');
 
-    
+  useEffect(()=>{
+    getAllGamesList();
+    getGameListByGenresId(4);
+  },[])
 
-    useEffect(()=>{
+  const getAllGamesList=()=>{
+    GlobalApi.getAllGames.then((resp)=>{
+      setAllGameList(resp.data.results)
       
-      GlobalApi.getPopularGame.then(resp=>{
-        console.log(resp)
-        setGameList(resp.data.results);
-       
     })
-    getGameList(4)
-    },[])
-    const getGameList=(genreId)=>{
-      console.log(genreId)
-      if(genreId!=0)
-      {
-        GlobalApi.getGameListByGenreId(genreId).then(resp=>{
-          console.log(resp)
-          setGameListByGenre(resp.data.results)
-      })
-      }
+  }
+
+  const getGameListByGenresId=(id)=>{
+ 
+    GlobalApi.getGameListByGenreId(id).then((resp)=>{
      
-        
-      
-      
-    }
+      setGameListByGenres(resp.data.results)
+    })
+  }
   return (
-    <div className='grid grid-cols-4 p-8'>
-        <div className='hidden md:flex'><SideNavGenreList 
-        selectedGenreId={(genreId)=>getGameList(genreId)} /></div>
-       {gameList?.length&&gameListByGenre?.length>0    ? 
-       <div className='md:col-span-3 col-span-4 px-3'>
-            <Slider game={gameListByGenre[0]} />
-            <TrendingGameList gameList={gameList}/>
-            <PopularGameList gameList={gameListByGenre} />
+    <div className='grid grid-cols-4 px-8'>
+        <div className='hidden md:block'>
+          <GenreList 
+          genereId={(genereId)=>getGameListByGenresId(genereId)}
+          selectedGenresName={(name)=>setSelctedGenresName(name)}
+           />
         </div>
-        :null}
+        <div className='col-span-4 md:col-span-3'>
+         {allGameList?.length>0&&gameListByGenres.length>0?
+         <div>
+          <Banner gameBanner={allGameList[0]} />
+          <TrendingGames gameList={allGameList} />
+          <GamesByGenresId gameList={gameListByGenres} 
+          selctedGenresName={selctedGenresName} />
+          </div>
+          :null}
+          </div>
+
     </div>
   )
 }
